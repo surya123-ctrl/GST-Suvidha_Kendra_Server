4c12fb7f72228901ea53a4caf00e026b52ac056a
Surya Pratap Singh <72143906+surya123-ctrl@users.noreply.github.com>
2024-01-04 16:13:16 +0530

    added passport.js authentication

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const nodemailer = require('nodemailer');
 const { check, validationResult } = require('express-validator');
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const session = require('express-session')
+const bcrypt = require('bcrypt');
+
+
 const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 5000;
 const USER_MAIL = process.env.USER_MAIL;
@@ -17,13 +23,13 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-
-
-let userCount = 0;
-app.use((req, res, next) => {
-    userCount++;
-    next();
-})
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 
@@ -51,6 +57,112 @@ const ItemSchema = new mongoose.Schema({
 
 const Item = mongoose.model('Form_Data', ItemSchema);
 
+const UserSchema = new mongoose.Schema({
+    username: { type: String, unique: true },
+    password: String,
+    name: String
+})
+const User = mongoose.model('User', UserSchema)
+passport.use(new localStrategy(
+    async function (username, password, done) {
+        try {
+            const user = await User.findOne({ username });
+
+            if (!user) {
+                return done(null, false);
+            }
+
+            const isPasswordValid = await bcrypt.compare(password, user.password);
+
+            if (!isPasswordValid) {
+                return done(null, false);
+            }
+
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        }
+    }
+));
+
+passport.serializeUser((user, done) => {
+    if (user) {
+        done(null, user.id);
+    } else {
+        done(null, false);
+    }
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then((user) => {
+            if (!user) {
+                return done(null, false);
+            }
+            return done(null, user);
+        })
+        .catch(err => done(err));
+});
+
+
+const isAuthenticated = (req, res, done) => {
+    if (req.user) {
+        return done();
+    }
+    return res.redirect('/');
+}
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({ message: 'Username already exists. Choose a different username.' });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = await User.create({ username: username, password: hashedPassword });
+        res.json({
+            status: 'success',
+            message: 'Sign up successful!',
+            data: {
+                user: newUser,
+            },
+
+        })
+    }
+    catch (error) {
+        console.log('Error creating user:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+
+});
+
+app.post('/login',
+    passport.authenticate('local', {
+        successRedirect: '/api/form',
+        failureRedirect: '/'
+    }),
+    function (req, res) {
+        res.json({ status: 'Logged in' });
+    });
+
+app.post('/logout', function (req, res, next) {
+    req.logout(function (err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+});
+
+let userCount = 0;
+app.use((req, res, next) => {
+    userCount++;
+    next();
+})
+
+
+
+
+
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -64,7 +176,7 @@ app.get('/', (req, res) => {
     connectDatabase();
 });
 
-app.get('/api/form', async (req, res) => {
+app.get('/api/form', isAuthenticated, async (req, res) => {
     try {
         const items = await Item.find();
         res.json({
